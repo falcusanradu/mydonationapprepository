780cefe34fde0d43f715b956686d864fa0326267
Radu Falcusan <falcusanradudan@gmail.com>
2018-03-03 16:13:00 +0200

    email adress validation

--- donationBackEnd/src/main/java/service/EmailService.java
+++ donationBackEnd/src/main/java/service/EmailService.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 import java.security.Security;
 
 import javax.mail.*;
+import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
@@ -36,49 +37,69 @@ public class EmailService {
 
         Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
 
-            emailService.sendSSLMessage(sendTo, emailSubjectTxt,
-                    emailMsgTxt, emailFromAddress);
-            System.out.println("Sucessfully Sent mail to All Users");
+        emailService.sendSSLMessage(sendTo, emailSubjectTxt,
+                emailMsgTxt, emailFromAddress);
+        System.out.println("Sucessfully Sent mail to All Users");
     }
 
     private void sendSSLMessage(String recipients[], String subject,
                                 String message, String from) throws MessagingException {
-        boolean debug = true;
 
-        Properties props = new Properties();
-        props.put("mail.smtp.host", SMTP_HOST_NAME);
+        boolean validationEmails = true;
+        if (!isValidEmailAddress(from)) validationEmails = false;
 
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.debug", "true");
-        props.put("mail.smtp.port", SMTP_PORT);
-        props.put("mail.smtp.socketFactory.port", SMTP_PORT);
-        props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
-        props.put("mail.smtp.socketFactory.fallback", "false");
-        Session session = Session.getDefaultInstance(props,
-                new javax.mail.Authenticator() {
 
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication("licenta.donation@gmail.com", "licenta.donation1");
-                    }
-                });
-
-        session.setDebug(debug);
-
-        Message msg = new MimeMessage(session);
-        InternetAddress addressFrom = new InternetAddress(from);
-        msg.setFrom(addressFrom);
-
-        InternetAddress[] addressTo = new InternetAddress[recipients.length];
-        for (int i = 0; i < recipients.length; i++) {
-            addressTo[i] = new InternetAddress(recipients[i]);
+        for (String sendAdress : sendTo) {
+            if (!isValidEmailAddress(sendAdress)) validationEmails = false;
         }
-        msg.setRecipients(Message.RecipientType.TO, addressTo);
+        if (validationEmails) {
+            boolean debug = true;
 
-        // Setting the Subject and Content Type
-        msg.setSubject(subject);
-        msg.setContent(message, "text/plain");
+            Properties props = new Properties();
+            props.put("mail.smtp.host", SMTP_HOST_NAME);
+
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.debug", "true");
+            props.put("mail.smtp.port", SMTP_PORT);
+            props.put("mail.smtp.socketFactory.port", SMTP_PORT);
+            props.put("mail.smtp.socketFactory.class", SSL_FACTORY);
+            props.put("mail.smtp.socketFactory.fallback", "false");
+            Session session = Session.getDefaultInstance(props,
+                    new javax.mail.Authenticator() {
+
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication("licenta.donation@gmail.com", "licenta.donation1");
+                        }
+                    });
+
+            session.setDebug(debug);
+
+            Message msg = new MimeMessage(session);
+            InternetAddress addressFrom = new InternetAddress(from);
+            msg.setFrom(addressFrom);
+
+            InternetAddress[] addressTo = new InternetAddress[recipients.length];
+            for (int i = 0; i < recipients.length; i++) {
+                addressTo[i] = new InternetAddress(recipients[i]);
+            }
+            msg.setRecipients(Message.RecipientType.TO, addressTo);
+
+            // Setting the Subject and Content Type
+            msg.setSubject(subject);
+            msg.setContent(message, "text/plain");
             Transport.send(msg);
+        }
+    }
 
+    public static boolean isValidEmailAddress(String email) {
+        boolean result = true;
+        try {
+            InternetAddress emailAddr = new InternetAddress(email);
+            emailAddr.validate();
+        } catch (AddressException ex) {
+            result = false;
+        }
+        return result;
     }
 
     private String[] convertListIntoArray(List<String> list) {
