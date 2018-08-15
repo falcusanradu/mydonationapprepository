53f03be6c87e586b4be20863807d5c067bf0489c
Radu Falcusan <falcusanradudan@gmail.com>
2018-08-15 15:48:01 +0300

    manage company frontend

--- frontend/src/app/pages/manage-company/manage-company.component.ts
+++ frontend/src/app/pages/manage-company/manage-company.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {CategoryEnum} from '../../models/interfaces';
+import {CategoryEnum, User} from '../../models/interfaces';
+import {BackendService} from '../../services/backend.service';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-manage-company',
@@ -10,11 +12,50 @@ export class ManageCompanyComponent implements OnInit {
 
   companyCategory: any[] = [];
 
-  constructor() {
+  disabled = true;
+  loggedUser: User;
+
+  constructor(private backendService: BackendService, private sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
     this.companyCategory = Object.keys(CategoryEnum).map(key => CategoryEnum[key]).filter(key => isNaN(key));
+    this.loggedUser = this.backendService.getSessionUser();
+    if (this.loggedUser.company) {
+      this.disabled = true;
+    } else {
+      this.disabled = false;
+      this.loggedUser.company = {
+        idCompany: null,
+        image: null,
+        description: '',
+        email: '',
+        address: '',
+        category: null,
+        userCompany: null,
+        name: ''
+      };
+    }
   }
+
+  trustImage(image) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64, ' + image);
+  }
+
+  edit() {
+    this.disabled = false;
+  }
+
+  cancel() {
+    this.disabled = true;
+  }
+
+  hasCompany() {
+    if (this.loggedUser.company.idCompany) {
+      return true;
+    }
+    return false;
+  }
+
 
 }
